a55fc1967c6812d8c8d313b87164a0479e622fee
Carlos Moyano <carlosmoya@gmail.com>
2019-01-02 16:39:16 -0300

    Test Button funcionality

--- app/src/main/java/com/syntax/justjava/MainActivity.java
+++ app/src/main/java/com/syntax/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.syntax.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    public void ingresarOrden(View view) {
+        mostrar(1);
+    }
+
+    private void mostrar(int numero) {
+
+        TextView NumberView = findViewById(R.id.cantidad_text_view);
+        NumberView.setText("" + numero);
+
+
+    }
 }
