3ec0dd7190c0543852bb096bde4fe0e3e38bb901
Carlos Moyano <carlosmoya@gmail.com>
2019-01-02 19:07:48 -0300

    Add a new Text whit price calculated.

--- app/src/main/java/com/syntax/justjava/MainActivity.java
+++ app/src/main/java/com/syntax/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.syntax.justjava;
 
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void ingresarOrden(View view) {
         mostrar(1);
+        displayPrice(1 * 5);
     }
 
     private void mostrar(int numero) {
@@ -22,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         TextView NumberView = findViewById(R.id.cantidad_text_view);
         NumberView.setText("" + numero);
 
-
     }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.precio_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
