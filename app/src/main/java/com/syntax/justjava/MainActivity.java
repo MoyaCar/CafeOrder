8b4256201b83ac0973ae74061c95fad722cfffc2
Carlos Moyano <carlosmoya@gmail.com>
2019-01-02 15:21:18 -0300

    Initial Commit, with base Layouts

---
+++ app/src/main/java/com/syntax/justjava/MainActivity.java
@@ -0,0 +1,15 @@
+package com.syntax.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
