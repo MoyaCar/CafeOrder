648657dadaa7a424daa74d59a9a99dcc1e0cfcbd
Carlos Moyano <carlosmoya@gmail.com>
2019-01-04 15:16:23 -0300

    Add New Message for order and other minor text changes.

--- app/src/main/java/com/syntax/justjava/MainActivity.java
+++ app/src/main/java/com/syntax/justjava/MainActivity.java
@@ -1,6 +1,6 @@
 package com.syntax.justjava;
 
-import android.icu.text.NumberFormat;
+
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -11,38 +11,43 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int CantidadDeCafe = 1;
+    String clientName = "Lulo Esperon";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+//construlle mensaje de orden y lo muestra
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void ingresarOrden(View view) {
-        String precio = "Precio total: $" + (CantidadDeCafe * 5) + "\n" + "Muchas Gracias!" ;
-
-        displayPrice(precio);
+        int price = calculatePrice();
+        String precio = createOrderSumary(price);
+        displayMessage(precio);
     }
 
-    private void mostrar(int numero) {
+//muestra mensaje de orden en pantalla
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void displayMessage(String string) {
+        TextView priceTextView = (TextView) findViewById(R.id.orden_view);
+        priceTextView.setText(string);
+    }
+
+    private void displayQuantity(int numero) {
 
         TextView NumberView = findViewById(R.id.cantidad_text_view);
         NumberView.setText("" + numero);
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private void displayPrice(String string) {
-        TextView priceTextView = (TextView) findViewById(R.id.precio_view);
-        priceTextView.setText(string);
-    }
 
     //Metodo para sumar cantidad de café
     public void sumar(View view) {
 
         if (CantidadDeCafe < 50) {
             CantidadDeCafe = CantidadDeCafe + 1;
-            mostrar(CantidadDeCafe);
+            displayQuantity(CantidadDeCafe);
         }
     }
 
@@ -50,10 +55,17 @@ public class MainActivity extends AppCompatActivity {
     public void restar(View view) {
         if (CantidadDeCafe > 1) {
             CantidadDeCafe = CantidadDeCafe - 1;
-            mostrar(CantidadDeCafe);
+            displayQuantity(CantidadDeCafe);
         }
 
     }
 
+    private int calculatePrice() {
+        return CantidadDeCafe * 5;
 
+    }
+
+    public String createOrderSumary(int precio) {
+        return clientName + "\n" + "Cantidad: " + CantidadDeCafe + "\n" + "Total: $" + precio + "\n" + "Muchas Graxiash";
+    }
 }
